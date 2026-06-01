0193854253c3caa1dd67e1030bf09f8ac8d00f81
Mohammad Amir Ansari <amiransari1721@gmail.com>
2026-06-01 11:40:01 +0400

    update tailwind to v4

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,28 +2,33 @@
     {{-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
-        </h2>
+    </h2>
     </x-slot> --}}
-    
-<div class="flex w-4/5 mx-auto justify-center space-x-2 bg-gray-800 p-4 border border-gray-500/50 rounded-md text-md">
-    <div class="flex items-center">
-        <input type="radio" name="options" id="html" class="hidden peer">
-        <label for="html" class="cursor-pointer rounded py-2 px-8 text-white transition-colors duration-200 peer-checked:bg-gray-600 peer-checked:text-white">All</label>
-    </div>
-    @foreach ($categories as $category)
-
-    <div class="flex items-center">
-        <input type="radio" name="options" id="html" class="hidden peer">
-        <label for="html" class="cursor-pointer rounded py-2 px-8 text-white transition-colors duration-200 peer-checked:bg-indigo-600 peer-checked:text-white">{{ $category->name }}</label>
-    </div>
-
-    @endforeach
-</div>
-
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xs sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                    <ul class="flex flex-wrap text-sm font-medium text-center text-body justify-center">
+
+                        <li class="me-2">
+                            <a href="#" class="inline-block px-4 py-2.5 text-white bg-brand rounded-base active" aria-current="page">All</a>
+                        </li>
+
+                        @foreach ($categories as $category)
+                        <li class="me-2">
+                            <a href="#" class="inline-block px-4 py-3 rounded-base hover:text-heading hover:bg-neutral-secondary-soft">{{ $category->name }}</a>
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xs sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
                 </div>
