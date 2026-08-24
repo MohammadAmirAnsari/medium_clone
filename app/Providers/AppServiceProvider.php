978f0f2756a436ed69cea45ad8daf2890d9dbff2
Mohammad Amir Ansari <amiransari1721@gmail.com>
2026-08-24 09:34:08 +0400

    use true

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (env('APP_ENV') !== 'local')
-            URL::forceHttps(1);
+            URL::forceHttps(true);
     }
 }
